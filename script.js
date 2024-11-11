6e2d78d7e37fb9223fa5fa8ebd67adec630d58df
AlyKoo <alykooabilhan@gmail.com>
2024-11-11 17:31:39 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let month = prompt("1-den 12-ge deyin san engizinz");
+var order = '';
+switch(month){
+    case '12':
+        alert('Hys');
+        order =  12;
+        break;
+    case '1':
+        alert('Hys');
+        order = 1;
+        break;
+    case '2':
+        alert('Hys');
+        order = 2;
+        break;
+    case '3':
+        alert('Koktem');
+        order =  3;
+        break;
+    case '4':
+        alert('Koktem');
+        order =  4;
+        break;
+    case '5':
+        alert('Koktem');
+        order =  5;
+        break;
+    case '6':
+        alert('Jaz');
+        order =  6;
+        break;
+    case '7':
+        alert('Jaz');
+        order =  7;
+        break;  
+    case '8':
+        alert('Jaz');
+        order =  8;
+        break;
+    case '9':
+        alert('Kuz');
+        order =  9;
+        break;   
+    case '10':
+        alert('Kuz');
+        order =  10;
+        break;   
+    case '11':
+        alert('Kuz');
+        order =  11;
+        break;         
+    default:
+        alert('1jyl 12 aidan turady 1den-12ge deyin san engiz');
+        order = Qate;
+}
---
+++ script2.js
@@ -0,0 +1,12 @@
+let a = prompt("1-shi sandy engiz");
+let b = prompt("2-shi sandy engiz");
+let c = prompt("3-shi sandy engiz");
+
+    if ('A > B') {
+       alert('A sani ulken')
+}   else ('A < B') 
+{
+       alert('B sani ulken')
+}   if ('A == B') {
+       alert('Eki san ten')
+}
